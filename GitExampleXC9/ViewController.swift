256a1366e109cc2d2d6b98daffc1e42581a73337
Joseph Howe <Jhowe_tgt@icloud.com>
2018-06-21 15:11:02 -0600

    Initial Commit

---
+++ GitExampleXC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExampleXC9
+//
+//  Created by Joseph Howe on 6/21/18.
+//  Copyright © 2018 Joseph Howe. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
